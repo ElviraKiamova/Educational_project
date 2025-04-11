b4bc110839c5d830db1c829e411061ef20f704e3
Kiamova Elvira <ela.kiyamova@yandex.ru>
2025-04-11 14:18:09 +0500

    feat: implement slider switching in the about section

--- Readme.md
+++ Readme.md
@@ -1,1 +1,2 @@
-тестовое задание по макету https://www.figma.com/design/iTdTi6IkwNE8Z76ADelRPm/Ингольф?node-id=317-660&t=OmvLgnRvqiA0SeD1-0
+Тестовое задание по макету:
+https://www.figma.com/design/iTdTi6IkwNE8Z76ADelRPm/Ингольф?node-id=317-660&t=OmvLgnRvqiA0SeD1-0
--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,54 @@ const containerElement = document.querySelector('.header__container');
 const popup = document.querySelector('.popup-application');
 const popupElement = document.querySelector('.popup-application');
 const closePopupButtonElement = document.querySelector('.popup-application__close');
+const prevButtonElement = document.querySelector('.about__button--prev');
+const nextButtonElement = document.querySelector('.about__button--next');
+const dotElements = document.querySelectorAll('.about__dot');
+const imgElement = document.querySelector('.about__picture img');
+const sourceElement = document.querySelector('.about__picture source');
+let currentSlide = 0;
+const desktopImages = [
+    './assets/about-desktop-image1.jpg',
+    './assets/about-desktop-image2.jpg',
+    './assets/about-desktop-image3.jpg'
+];
+const mobileImages = [
+    './assets/about-mobile-image1.jpg',
+    './assets/about-mobile-image2.jpg',
+    './assets/about-mobile-image3.jpg'
+];
+
+
+
+function updateSlide(index) {
+    imgElement.src = desktopImages[index];
+    sourceElement.srcset = mobileImages[index];
+    dotElements.forEach(dot => dot.classList.remove('about__dot_active'));
+    dotElements[index].classList.add('about__dot_active');
+    currentSlide = index;
+}
+
+prevButtonElement.addEventListener('click', () => {
+    let newIndex = currentSlide - 1;
+    if (newIndex < 0) newIndex = desktopImages.length - 1;
+    updateSlide(newIndex);
+});
+
+nextButtonElement.addEventListener('click', () => {
+    let newIndex = currentSlide + 1;
+    if (newIndex >= desktopImages.length) newIndex = 0;
+    updateSlide(newIndex);
+});
+
+dotElements.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        updateSlide(index);
+    });
+});
+
+
+
+
 
 burgerElement.addEventListener('click', () => {
   containerElement.classList.toggle('header__container_active');
